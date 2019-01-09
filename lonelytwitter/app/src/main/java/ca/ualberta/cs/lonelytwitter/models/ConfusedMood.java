8159bbf9fe9234ae0285cc0aeace3ffc2211b93e
kthiesse <43151242+kthiesse@users.noreply.github.com>
2019-01-08 19:37:35 -0700

    Add files via upload

---
+++ lonelytwitter/app/src/main/java/ca/ualberta/cs/lonelytwitter/models/ConfusedMood.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cs.lonelytwitter.models;
+
+public class ConfusedMood extends CurrentMood {
+    private string date;
+    public string getDate() {
+        return date;
+    }
+
+    private void setDate(string date) {
+        this.date = date;
+    }
+
+}
